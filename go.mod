c79fb8f2a70e4f8a1e946bcd71eead26330ae15f
samiam2013 <sam@myres.dev>
2022-06-21 18:42:41 -0500

    first working code

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/samiam2013/rylr896
 
 go 1.18
+
+require github.com/albenik/go-serial v1.2.0
+
+require (
+	github.com/creack/goselect v0.1.0 // indirect
+	github.com/sirupsen/logrus v1.8.1
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"github.com/albenik/go-serial"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	mode := &serial.Mode{
+		BaudRate: 115200,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: 0,
+	}
+	port, err := serial.Open("/dev/serial0", mode)
+	if err != nil {
+		logrus.WithError(err).Error("failed to open /dev/serial0")
+	}
+
+	// TODO break this into it's own function?
+	n, err := port.Write([]byte("AT\r\n"))
+	if err != nil {
+		logrus.WithError(err).Fatal("Failed to send message")
+	}
+	logrus.Infof("Sent %v bytes\n", n)
+
+	response := make([]byte, 1000)
+	for {
+		buf := make([]byte, 100)
+		n, err := port.Read(buf)
+		if err != nil {
+			logrus.WithError(err).Error("Could not read port")
+			break
+		}
+		if n == 0 {
+			logrus.Info("EOF")
+			break
+		}
+		response = append(response, buf...)
+	}
+	logrus.Infof("response:'%v'", string(response))
+
+}
