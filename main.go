f26bacd8072980e8f0b18db7947d9ccc16947a9a
samiam2013 <sam@myres.dev>
2022-06-22 13:31:21 -0500

    working code (recieves beacon)

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		BaudRate: 115200,
 		Parity:   serial.NoParity,
 		DataBits: 8,
-		StopBits: 0,
+		StopBits: serial.OneStopBit,
 	}
 	port, err := serial.Open("/dev/serial0", mode)
 	if err != nil {
@@ -26,15 +26,55 @@ func main() {
 	}()
 
 	logrus.Infof("response:'%v'", sendCommand(port, "AT"))
-	logrus.Infof("response:'%v'", sendCommand(port, "AT+IPR?"))
+	// TODO ? check if the response ^ to that was '+OK'
+
+	cmds := []string{
+		"AT+PARAMETER=12,7,1,7",
+		"AT+BAND=915000000",
+		"AT+ADDRESS=1",
+		"AT+NETWORKID=6",
+		"AT+CRFOP=15",
+	}
+	for _, cmd := range cmds {
+		logrus.Infof("'%s' ran, result: '%s'", cmd, sendCommand(port, cmd))
+	}
+
+	for {
+		if n, err := port.ReadyToRead(); n > 0 && err == nil {
+			readBuf := make([]byte, 1000)
+			for {
+				buf := make([]byte, 100)
+				n, err := port.Read(buf)
+				if err != nil {
+					logrus.WithError(err).Error("Could not read port")
+				}
+				if n == 0 {
+					break
+				}
+				readBuf = append(readBuf, buf...)
+			}
+			logrus.Infof("Data from port: %s", string(readBuf))
+		}
+	}
 }
+
+// func sendMessage(p serial.Port, msg string) (string, error) {
+// 	// TODO sanitize (or error? on new lines)
+// 	response := sendCommand(p, "AT+SEND="+msg)
+// 	if !strings.HasPrefix(response, "+OK") {
+// 		return "", fmt.Errorf("not '+OK' sending message: %s", response)
+// 	}
+// 	return response, nil
+// }
 
 func sendCommand(p serial.Port, cmd string) string {
 	n, err := p.Write([]byte(cmd + "\r\n"))
 	if err != nil {
 		logrus.WithError(err).Fatal("Failed to send message")
 	}
-	logrus.Infof("Sent %v bytes\n", n)
+	logrus.Infof("Sent %v bytes: '%s'\n", n, cmd)
+
+	//time.Sleep(time.Millisecond * 1000)
 
 	response := make([]byte, 1000)
 	for {
@@ -55,3 +95,5 @@ func sendCommand(p serial.Port, cmd string) string {
 	}
 	return string(response)
 }
+
+// map strings in the pattern /\+ERR=([d]{1-2})/ based on the number to a string
